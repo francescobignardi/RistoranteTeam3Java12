3986f1db1badab9108a82d9981d9c9bcc5b7b6ba
RAFFAELE <raffaelefusco0101@outlook.it>
2023-05-02 10:56:39 +0200

    RAFFAELE, colori aggiunti in enum"Colori_Ansi.java"

--- src/Colori_ANSI.java
+++ src/Colori_ANSI.java
@@ -1,10 +1,17 @@
-public class Colori_ANSI {
-    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
-    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+public enum Colori_ANSI {
+    YELLOW("\u001B[43m"), RED("\u001B[41m"), GREEN("\u001B[42m"), BLUE("\u001B[44m"), CYAN("\u001B[46m"), BLACK("\u001B[40m"), PURPLE("\u001B[45m"), WHITE("\u001B[47m");
+
+    String colorCode;
+
+    Colori_ANSI(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public String getColorCode() {
+        return this.colorCode;
+    }
+
+    public String getColor(String colorCode) {
+        return Colori_ANSI.this.colorCode;
+    }
 }
